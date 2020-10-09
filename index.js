584c7fdcff6601c30ae655cb3ede703d7a0e0cb2
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-10-09 16:56:11 +0200

    Say 2.2

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const say = what => console.log(what);
+const say2 = what => console.log(what);
 
 const sayHello = () => say(`Hello!`);
-const sayHello2 = () => say(`Hello!`);
+const sayHello2 = () => say2(`Hello!`);
 
 const bye = () => say('Bye');
 sayHello();
